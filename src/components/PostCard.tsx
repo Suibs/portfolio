cd3d79060dec042cb9951f48f4c7fc099c2afdd9
Suibin Hong <suibin.hong@outlook.com>
2020-02-19 16:54:52 +0000

    Blog

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -233,12 +233,6 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
               <AuthorNameTooltip className="author-name-tooltip">
                 {post.frontmatter.author.id}
               </AuthorNameTooltip>
-              <Link css={StaticAvatar} to={`/author/${_.kebabCase(post.frontmatter.author.id)}/`}>
-                <AuthorProfileImage
-                  src={post.frontmatter.author.avatar.children[0].fixed.src}
-                  alt={post.frontmatter.author.id}
-                />
-              </Link>
             </AuthorListItem>
           </AuthorList>
           <ReadingTime>{post.timeToRead} min read</ReadingTime>
